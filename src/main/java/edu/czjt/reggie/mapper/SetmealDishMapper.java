96727df8a169886a11d14f60db8daacbabf042ce
liumiao <136035209+1563327@users.noreply.github.com>
2023-06-15 09:30:17 +0800

    Update SetmealDishMapper.java

--- src/main/java/edu/czjt/reggie/mapper/SetmealDishMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/SetmealDishMapper.java
@@ -1,5 +1,9 @@
 package edu.czjt.reggie.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.SetmealDish;
+import org.apache.ibatis.annotations.Mapper;
 
-public interface SetmealDishMapper  {
+@Mapper
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
 }
