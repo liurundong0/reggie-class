5bdb4fdc9333edbcc27214645f79aefbe18fc188
liumiao <136035209+1563327@users.noreply.github.com>
2023-06-15 09:35:53 +0800

    Update OrderService.java

--- src/main/java/edu/czjt/reggie/service/OrderService.java
+++ src/main/java/edu/czjt/reggie/service/OrderService.java
@@ -1,8 +1,15 @@
 package edu.czjt.reggie.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.Orders;
 
+public interface OrderService extends IService<Orders> {
 
-public interface OrderService  {
+    /**
+     * 用户下单
+     * @param orders
+     */
+    public void submit(Orders orders);
 
-
+    public void submitAgain(Orders orders);
 }
