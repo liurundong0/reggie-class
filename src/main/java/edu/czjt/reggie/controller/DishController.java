056d8eadfba75a5c1ee52416442326bcacc81aad
Wei246 <135291290+Wei246@users.noreply.github.com>
2023-06-15 09:38:56 +0800

    Update DishController.java

--- src/main/java/edu/czjt/reggie/controller/DishController.java
+++ src/main/java/edu/czjt/reggie/controller/DishController.java
@@ -1,7 +1,7 @@
 package edu.czjt.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import edu.czjt.reggie.common.R;
 import edu.czjt.reggie.dto.DishDto;
@@ -14,10 +14,10 @@ import edu.czjt.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -128,3 +128,70 @@ public class DishController {
 
         return dishDto;
     }
+    /**
+     * Created by wangweiwei
+     */
+    
+    @PostMapping()
+    public R<String> save(@RequestBody DishDto dishDto) {
+        log.debug("保存dish: {}", dishDto.toString());
+
+        dishService.saveWithFlavor(dishDto);
+
+        return R.success("新增菜品成功");
+    }
+
+    @PutMapping("/status/{status}")
+    public R<String> status(@PathVariable("status") Integer status, @RequestBody String ids) {
+        log.debug("修改菜品ids:{} 的状态为：{}。", ids, status);
+
+        List<Long> idList = Arrays.stream(ids.split(","))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        UpdateWrapper<Dish> dishUpdateWrapper = new UpdateWrapper<>();
+        dishUpdateWrapper.in("id", idList).set("status", status);
+
+        dishService.update(dishUpdateWrapper);
+        return R.success("更新成功");
+    }
+
+    @PutMapping()
+    @Transactional
+    public R<String> update(@RequestBody DishDto dishDto) {
+        log.debug("更新dishDto:{}", dishDto.toString());
+
+        dishService.updateById(dishDto);
+
+        // 按照dishid删除dishflavor
+        dishFlavorService.removeByDishId(dishDto.getId());
+
+        List<DishFlavor> dishFlavors = dishDto.getFlavors().stream().map((item) -> {
+            item.setDishId(dishDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(dishFlavors);
+
+        // dishFlavorService.updateBatchById(dishFlavors);
+
+        return R.success("更新成功");
+    }
+
+    @DeleteMapping()
+    @Transactional
+    public R<String> delete(@RequestParam("ids") String ids) {
+        log.info("删除菜名ids：{}", ids);
+        List<Long> idList = Arrays.stream(ids.split(","))
+                .map(item->{
+                    Long id = Long.valueOf(item);
+                    dishFlavorService.removeByDishId(id);
+                    dishService.removeById(id);
+                    return id;
+                })
+                .collect(Collectors.toList());
+        return R.success("删除菜品成功。");
+    }
+
+}
+
