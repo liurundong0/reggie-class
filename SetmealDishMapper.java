f5575be7be59dcbd29377951f58124e0cad06b26
Past0rGrem <135928853+Past0rGrem@users.noreply.github.com>
2023-06-16 09:24:12 +0800

    Add files via upload

---
+++ SetmealDishMapper.java
@@ -0,0 +1,8 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.SetmealDish;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+}
---
+++ SetmealMapper.java
@@ -0,0 +1,8 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Setmeal;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface SetmealMapper extends BaseMapper<Setmeal> {
+}
