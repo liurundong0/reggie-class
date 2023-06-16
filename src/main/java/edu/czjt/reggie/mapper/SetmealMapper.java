5190f6b02339d1ed54fe501d05e5ab010999cae5
Past0rGrem <135928853+Past0rGrem@users.noreply.github.com>
2023-06-16 09:28:20 +0800

    Update SetmealMapper.java

--- src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
@@ -1,5 +1,9 @@
 package edu.czjt.reggie.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Setmeal;
+import org.apache.ibatis.annotations.Mapper;
+
 
 public interface SetmealMapper  {
 }
